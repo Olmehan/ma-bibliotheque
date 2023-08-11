03f0139c1b6528f452cf5618bf01bdb34ac726b4
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-11 01:29:23 -0400

    ajout reponce serveur

--- BackEndExpress/src/server.js
+++ BackEndExpress/src/server.js
@@ -1,31 +1,76 @@
 import express from 'express';
-import sql from 'mssql';
+const sql = require("mssql");
 
 const app = express();
+app.use(express.json());
 const mssqlConfig = {
-    user: 'DESKTOP-QV0D28Q\Oleksii',
-    password: '',
+    // user: 'Oleksii',
+    user: 'TestUser',
+    // user: 'DESKTOP-QV0D28Q\\Oleksii',
+    password: 'test978A',
     server: 'localhost',
-    database: 'Bibliotheque_films'
+    options: {
+    //     Trusted_Connection: true,
+    //     encrypt: false,
+        trustServerCertificate: true,
+    //     // Integrated Security=SSPI
+    }
 };
 
 app.get('/hello', (requete, reponce) => reponce.send('Salut !'));
 
-app.get('/base', (requete, reponce) => {
+app.get('/', async(requete, reponce) => {
+    console.log('connexion...');
+
     sql.connect(mssqlConfig, (erreur) => {
-        if(erreur) console.log(erreur);
+        if(erreur){
+            console.log('...erreur de connexion...');
+            console.log(erreur);
+        } else {
+            console.log("sql query ... demande de donnees ...");
+            const result = sql.query(`select NomFilm as Films, Duree, DatePremiere from [Bibliotheque_films].[dbo].[Films]`, (err, donnees) => {
+                if(err) console.log(err);
 
-        //creation Request object
-        var request = new sql.Request();
+                //reponce
+                var tabl = new Array();
+                for(let i = 0; i < donnees.recordset.length; i++){
+                    var film = donnees.recordset[i].Films;
+                    var duree = donnees.recordset[i].Duree;
+                    var datePremiere = donnees.recordset[i].DatePremiere;
+                    tabl.push({'Film':film, 'Duree':duree, 'Date premiere':datePremiere});
+                }
+                console.log('--Donnees--');
+                console.log(tabl);
+                reponce.send(tabl);
+            });
+        }
+    })
+})
 
-        //query à bd et obtenir des données
-        request.query('select * from Films', (err, donnees) => {
-            if(err) console.log(err);
-        })
+app.get('/films', async(requete, reponce) => {
+    console.log("eesaye de connect...");
 
-        //reponce
-        reponce.send(donnees);
-    });
+    sql.connect(mssqlConfig, (erreur) => {
+        if(erreur){
+            console.log("... erreur de connexion ...");
+            console.log(erreur);
+        } else {
+            console.log("sql query ... demande de donnees ...");
+            const result = sql.query(`select NomFilm as Films from [Bibliotheque_films].[dbo].[Films]`, (err, donnees) => {
+                if(err) console.log(err);
+
+                //reponce
+                var tabl = new Array();
+                for(let i = 0; i < donnees.recordset.length; i++){
+                    var film = donnees.recordset[i].Films;
+                    tabl.push({'Film':film});
+                }
+                console.log('--Donnees--');
+                console.log(tabl);
+                reponce.send(tabl);
+            });
+        }
+    })
 });
 
 app.listen(8000, () => console.log('Écoute le port 8000'));
