ef0933254a00bf5511f7262a6bad586575c3a30d
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-11 23:55:25 -0400

    ajout pageActeurs

---
+++ src/pages/PageActeurs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const PageActeurs = () => {
+
+    return (
+        <Container>
+            <h1>Bienvenue dans la page des acteurs</h1>
+        </Container>
+    )
+}
