603052e595fada8145a09bedd3c2fe4404544313
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-12 00:16:23 -0400

    ajout PageRealisateurs

---
+++ src/pages/PageRealisateurs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const PageRealisateurs = () => {
+
+    return (
+        <Container>
+            <h1>Bienvenue dans la page des realisateurs</h1>
+        </Container>
+    )
+}
