35f9ddbb8fa3b8d1323183cc11bfcbde48907347
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-12 00:12:02 -0400

    ajout paths acteurs et realisateurs

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,8 @@
 // import { Accueil } from './components/Accueil';
 import { PageAccueil } from './pages/PageAccueil';
 import { PageFilms } from './pages/PageFilms';
+import { PageActeurs } from './pages/PageActeurs';
+import { PageRealisateurs } from './pages/PageRealisateurs';
 import { Erreur404 } from './pages/Erreur404';
 
 const AppRoutes = [
@@ -11,6 +13,14 @@ const AppRoutes = [
     {
         path: '/pageFilms',
         element: <PageFilms />
+    },
+    {
+        path: '/pageActeurs',
+        element: <PageActeurs />
+    },
+    {
+        path: '/pageRealisateurs',
+        element: <PageRealisateurs />
     },
     {
         path: '*',
