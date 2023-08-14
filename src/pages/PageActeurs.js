8ad59f00c2c2e4be680043b70de82a03a555c52d
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-13 22:51:43 -0400

    ajout table acteurs

--- src/pages/PageActeurs.js
+++ src/pages/PageActeurs.js
@@ -1,11 +1,46 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Table } from "react-bootstrap";
+import { FormatDate } from "../components/FormatDate";
 
 export const PageActeurs = () => {
+    const[listActeurs, setListActeurs] = useState([]);
+
+    useEffect(() => {
+        getTousActeurs();
+    }, []);
+
+    async function getTousActeurs(){
+        const result = await fetch('http://localhost:8000/acteurs');
+        const acteurs = await result.json();
+        console.log('Acteurs: ');
+        console.log(acteurs);
+
+        setListActeurs(acteurs);
+    }
 
     return (
         <Container>
             <h1>Bienvenue dans la page des acteurs</h1>
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Date naissance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Array.isArray(listActeurs) ? listActeurs.map((a, index) => (
+                        <tr key={a.idActeur}>
+                            <td>{index + 1}</td>
+                            <td>{a.nomActeur}</td>
+                            <td>{a.prenomActeur}</td>
+                            <td><FormatDate donneesDateTime={a.dateNaissance} /></td>
+                        </tr>
+                    )) : []}
+                </tbody>
+            </Table>
         </Container>
     )
 }
