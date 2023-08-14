5be67e795e633cc0f10b1e36dccc2612a5c4b8e6
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-08-13 20:57:48 -0400

    ajout page de film

---
+++ src/pages/PageUnFilm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const PageUnFilm = () => {
+    return (
+        <Container>
+            <h3>Page de film</h3>
+        </Container>
+    )
+}
